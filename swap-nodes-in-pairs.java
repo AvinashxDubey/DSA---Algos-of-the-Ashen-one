e309872622abe2e28fc6ecbb8474d72ec577faa8
Avinash Dubey <speedofreigns619@gmail.com>
2025-02-26 17:08:24 +0530

    Add swap-nodes-in-pairs (java)

---
+++ swap-nodes-in-pairs.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode dummy = new ListNode(0, head);
+        ListNode temp = dummy;
+
+        while(temp.next!=null && temp.next.next!=null){
+            ListNode prev = temp.next;
+            ListNode curr = temp.next.next;
+            prev.next = curr.next;
+            curr.next = prev;
+
+            temp.next = curr;
+            temp = prev;
+
+        }
+        return dummy.next;
+    }
+}
