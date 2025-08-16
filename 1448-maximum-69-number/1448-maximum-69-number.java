050b969292d6a9237e3ad7dd824d915ca917322c
Avinash Dubey <119841049+AvinashxDubey@users.noreply.github.com>
2025-08-16 21:58:58 +0530

    Time: 0 ms (100%), Space: 40.7 MB (44.6%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maximum69Number (int num) {
         String str = String.valueOf(num);
-        int[] nums = new int[str.length()];
+        // int[] nums = new int[str.length()];
+        // for(int i=0; i<nums.length; i++){
+        //     nums[i] = str.charAt(i)-'0';
+        // }
+        char[] nums = str.toCharArray();
+
         for(int i=0; i<nums.length; i++){
-            nums[i] = str.charAt(i)-'0';
-        }
-        System.out.println(Arrays.toString(nums));
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==6){
-                nums[i]= 9;
+            if(nums[i]=='6'){
+                nums[i]= '9';
                 break;
             }
         }
 
-        int res = 0;
-        for(int i=0; i<nums.length; i++){
-            res = res*10 + nums[i];
-        }
+        // int res = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     res = res*10 + nums[i];
+        // }
 
-        return res;
+        return Integer.valueOf(new String(nums));
     }
 }
