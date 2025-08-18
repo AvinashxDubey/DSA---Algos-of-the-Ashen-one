8df43120c5340125a17816f9c86d1a0f5c9957d8
Avinash Dubey <119841049+AvinashxDubey@users.noreply.github.com>
2025-08-18 19:10:34 +0530

    Time: 6 ms (27.1%), Space: 41.3 MB (98.68%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,29 +1,54 @@
+// class Solution {
+//     public List<String> summaryRanges(int[] nums) {
+//         List<String> res = new ArrayList<>();
+//         if (nums.length <= 0)
+//             return res;
+//         int[] temp = new int[2];
+//         temp[0] = nums[0];
+//         temp[1] = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1] + 1) {
+//                 temp[1] = nums[i];
+//             } else {
+//                 if (temp[0] == temp[1]) {
+//                     res.add(String.valueOf(temp[0]));
+//                 } else {
+//                     res.add(String.valueOf(temp[0]) + "->" + String.valueOf(temp[1]));
+//                 }
+//                 temp[0] = nums[i];
+//                 temp[1] = nums[i];
+//             }
+//         }
+
+//         if (temp[0] == temp[1]) {
+//             res.add(String.valueOf(temp[0]));
+//         } else {
+//             res.add(String.valueOf(temp[0]) + "->" + String.valueOf(temp[1]));
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<>();
         if (nums.length <= 0)
             return res;
-        int[] temp = new int[2];
-        temp[0] = nums[0];
-        temp[1] = nums[0];
+        int start = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1] + 1) {
-                temp[1] = nums[i];
-            } else {
-                if (temp[0] == temp[1]) {
-                    res.add(String.valueOf(temp[0]));
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start==nums[i - 1]) {
+                    res.add(String.valueOf(start));
                 } else {
-                    res.add(String.valueOf(temp[0]) + "->" + String.valueOf(temp[1]));
+                    res.add(start + "->" + nums[i - 1]);
                 }
-                temp[0] = nums[i];
-                temp[1] = nums[i];
+                start = nums[i];
             }
         }
 
-        if (temp[0] == temp[1]) {
-            res.add(String.valueOf(temp[0]));
+        if ( start==nums[nums.length-1]) {
+            res.add(String.valueOf(start));
         } else {
-            res.add(String.valueOf(temp[0]) + "->" + String.valueOf(temp[1]));
+            res.add(start + "->" + nums[nums.length - 1]);
         }
         return res;
     }
