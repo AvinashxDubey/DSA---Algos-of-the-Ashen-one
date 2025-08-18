545f69a6bb04a3160eee7069ff0d38f5bbf96aa9
Avinash Dubey <119841049+AvinashxDubey@users.noreply.github.com>
2025-08-18 10:08:30 +0530

    Time: 0 ms (100%), Space: 40.7 MB (99.31%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        /**
+            a classic example of dfs
+            we solve this using recursive dfs approach
+            just swap the left and right elements recursively
+         */
+        if(root==null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        return root;
+    }
+}
