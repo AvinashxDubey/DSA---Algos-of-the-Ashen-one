51950795da3d58603abe859f7a4919ecc72fcd4c
Avinash Dubey <speedofreigns619@gmail.com>
2025-08-26 15:39:17 +0530

    Add kth-largest-element-in-a-stream (java)

---
+++ kth-largest-element-in-a-stream.java
@@ -0,0 +1,25 @@
+class KthLargest {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    int k;
+    int[] nums;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.nums = nums;
+        for(int num : nums){
+            minHeap.offer(num);
+            if(minHeap.size()>k) minHeap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if(minHeap.size()>k) minHeap.poll();
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
