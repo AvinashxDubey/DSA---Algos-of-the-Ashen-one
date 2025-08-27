f023dbccd2da45427d41c1b87126647a71785794
Avinash Dubey <119841049+AvinashxDubey@users.noreply.github.com>
2025-08-27 10:33:29 +0530

    Time: 65 ms (23.22%), Space: 58 MB (62.94%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.java
@@ -1,4 +1,12 @@
 class Solution {
+    /**
+        Simple enough
+        create a map to track occurences of elements
+        then create a pq storing key pairs of map based on least frequency
+        iterate over the pq and remove elements with freq less than or equal to k-alreadyRemoved
+        when freq becomes greater than that we get outta the loop
+        and return the size of the pq
+     */
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         int removed = 0;
         Map<Integer, Integer> map = new HashMap<>();
