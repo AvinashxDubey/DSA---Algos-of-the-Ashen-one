54c3658dfff18bf2f44fd3aaf1feb7529e5557b6
Avinash Dubey <speedofreigns619@gmail.com>
2025-08-30 17:31:29 +0530

    Time: 9 ms (23.96%), Space: 40.1 MB (91.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
+    /**
+        0 1   2     3     4     5     6
+        0 1 0+1=1 1+1=2 1+2=3 2+3=5 3+5=8...
+        F(6) = F(5) + F(4) = 5+3 = 8
+     */
+
     public int fib(int n) {
-        int sol = 0;
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
         return fib(n-1) + fib(n-2);
     }
 }
