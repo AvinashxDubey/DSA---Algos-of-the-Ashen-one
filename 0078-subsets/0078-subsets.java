ac9cad228cdc87af4a6a88c49146ce4988ce90c0
Avinash Dubey <speedofreigns619@gmail.com>
2025-09-18 11:35:51 +0530

    Time: 0 ms (100%), Space: 42.7 MB (83.81%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+
+        createSubsets(nums, 0, res, ls);
+        return res;
+    }
+
+    public static void createSubsets(int[] nums, int idx, List<List<Integer>> res, List<Integer> ls){
+        if(idx==nums.length){
+            res.add(new ArrayList<>(ls));
+            return;
+        }
+        
+        // choose the curr num
+        ls.add(nums[idx]);
+        createSubsets(nums, idx+1, res, ls);
+        
+        // or not choose the curr num
+        ls.remove(ls.size()-1);
+        createSubsets(nums, idx+1, res, ls);
+    }
+}
