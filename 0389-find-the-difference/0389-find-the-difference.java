540cda2192f166353b23f5645d92d4887cfdc985
Avinash Dubey <speedofreigns619@gmail.com>
2025-10-09 10:52:31 +0530

    Time: 1 ms (99.32%), Space: 42.1 MB (18.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int m = s.length(), n =t.length();
+        int count = 0;
+        for(char ch : s.toCharArray()){
+            count-=(int)ch;
+        }
+
+        for(char ch : t.toCharArray()){
+            count+=(int)ch;
+        }
+        return (char)count;
+    }
+}
