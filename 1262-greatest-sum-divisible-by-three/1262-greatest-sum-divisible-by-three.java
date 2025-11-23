f25dd802f5b29983239b33737b4e561e52483103
Avinash Dubey <speedofreigns619@gmail.com>
2025-11-23 20:23:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+        we just need to keep track of the remainder 
+        curr sum % 3 and at the end check if taking the element or leaving it gives out the max valid sum 
+     */
+    public int maxSumDivThree(int[] nums) {
+        Integer[][] dp = new Integer[nums.length][3];
+        return solve(nums, 0, 0, dp);
+    }
+
+    public int solve(int[] nums, int idx, int rem, Integer[][] dp){
+        if(idx==nums.length){
+            if(rem==0) return 0;
+            return Integer.MIN_VALUE;
+        }
+        
+        int pick = nums[idx] + solve(nums, idx+1, (nums[idx]+rem)%3, dp);
+
+        int notPick = solve(nums, idx+1, rem, dp);
+
+        return Math.max(pick, notPick);
+    }
+}
